f9601d91267f88dc1d656691fcca80291669150f
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-12 10:53:55 +0530

    variables in javascript

---
+++ 03_Javascript/01_Variables.js
@@ -0,0 +1,20 @@
+var myName = "Alice";
+//--->can be reassigned and are only available inside the function they're created in. It's functionally scoped. 
+
+const newName = value;
+//--->cannot be reassigned and not accessible before they appear within the code. It's block scoped.
+
+let nextName = value;
+//can be reassigned but it's similar to const i.e, block scoped.
+
+/* NOTE */
+//--->If variables are not created inside a function or block they are globally scoped.
+
+/*******
+ 
+    **What is Block?
+    --->A block is a set of opening and closing curly brackets.
+    These blocks are often used to group statements together, 
+    such as in control flow statements (like if, else, for, while, etc.) or to define the scope of variables.
+
+ ******/
