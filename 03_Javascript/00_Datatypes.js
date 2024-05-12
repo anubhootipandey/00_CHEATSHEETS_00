9f944d39cb261186e8eb19848d227b1044617988
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-12 10:46:15 +0530

    datatype

---
+++ 03_Javascript/00_Datatypes.js
@@ -0,0 +1,21 @@
+/*****************
+ 
+Javascript has two types of Datatypes:
+
+//Primitive Datatypes:-
+
+1.String---> "Hello" etc.
+2.Number---> 5,6,3,etc.
+3.Symbol--->used as an identifier for object properties.
+4.Boolean--->true or false
+5.Undefined--->a variable that has not been assigned a value, is undefined.
+6.Null--->represents null i.e, no value at all.
+
+//Non-Primitive Datatypes:-
+
+1.Object--->instance through which we can access members
+2.Arrays--->group of similar values
+3.RegExp--->represents regular expression
+
+*****************/
+
