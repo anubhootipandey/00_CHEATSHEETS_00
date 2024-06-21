43fef9e94270e270fb5e0d0d7cf28f836a983836
anubhootipandey <anubhootipandey05@gmail.com>
2024-06-21 21:36:35 +0530

    Datatypes

--- 03_Javascript/01_Datatypes.js
+++ 03_Javascript/01_Datatypes.js
@@ -15,7 +15,34 @@ Javascript has two types of Datatypes:
 
 1.Object--->instance through which we can access members
 2.Arrays--->group of similar values
-3.RegExp--->represents regular expression
 
 *****************/
+
+//-----------------------------Primitive Datatypes-----------------------------//
+
+// 1.String
+let text = 'Hello!';
+
+// 2.Number
+let number = 42;
+
+// 3.Boolean
+let isJavaScriptFun = true;
+
+// 4.Undefined
+let something;
+
+// 5.Null
+let empty = null;
+
+// 6.Symbol
+let symbol = Symbol("unique");
+
+//---------------------------Non-Primitive Datatypes-----------------------------//
+
+// 1. Object
+let person = { name: 'Alice', age: 30 };
+
+// 2.Array
+let colors = ['red', 'green', 'blue'];
 
